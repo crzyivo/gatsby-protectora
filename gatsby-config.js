3b4405a8cd67e174905171749e98ad6cb99c2b3f
Iván Escuín <escuin100@gmail.com>
2019-03-19 17:24:54 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `APAP Bajo Aragón`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@crzyivo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
